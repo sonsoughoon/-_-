ba5cd20d012d54890ced71023e69c8efd40e0b5f
sonsoughoon <gkgkgktjdgns@gmail.com>
2022-05-02 09:25:57 +0900

    초기ㅁㅁ

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//1234
+	//12.34
+	int a;
+	int b;
+	float c;
+	float d;
+
+	a = 1;
+	b = 2;
+	c = 3.1;
+	d = 4.1;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << c - d << endl;
+	cout << c + d << endl;
+	cout << c * d << endl;
+	cout << c / d << endl;
+
+	return 0;
+
+
+}
